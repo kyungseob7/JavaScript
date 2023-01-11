0698258a3a1d544ae8a7b5b5629dc26375a5267d
kyungseob7 <kseob7@naver.com>
2023-01-11 22:22:42 +0900

    20230111 최종완료

--- client/chapter/dom/06.event.js
+++ client/chapter/dom/06.event.js
@@ -12,7 +12,7 @@
 
 const first = getNode('.first');
 const second = getNode('.second');
-const ground= getNode('.ground');
+const ground = getNode('.ground');
 const ball = getNode('.ball');
 
 
@@ -20,35 +20,66 @@ function handler(){
   console.log('hit !');
 
   // css('.second','display','none')
-
-
 }
 
-// first.addEventListener('click', handler);
+first.addEventListener('click', handler);
 
 
 
 // const off = bindEvent('.first','click',handler);
-
 // bindEvent('.second','click',off);
 
 
-ground.addEventListener('mousemove',function(e){
+ground.addEventListener('click',function(e){
   
-  console.log(e.offsetX,e.offsetY);
+  console.log(e.offsetX, e.offsetY);
 
-  ball.style.transform = `translate(${e.offsetX}px, ${e.offsetY}px)`;
+  ball.style.transform = `translate(${e.offsetX - ball.offsetWidth / 2}px,${e.offsetY - ball.offsetHeight / 2}px)`;
+
 })
 
 
-ground.addEventListener('mousemove',function(e){
-  console.log(e.offsetX,e.offsetY);
-})
+function debounce(callback, limit = 100) {
+  let timeout
+  return function(...args) {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+          callback.apply(this, args)
+      }, limit)
+  }
+}
+
+function throttle(callback, limit = 100) {
+  let waiting = false
+  return function() {
+      if(!waiting) {
+          callback.apply(this, arguments)
+          waiting = true
+          setTimeout(() => {
+              waiting = false
+          }, limit)
+      }
+  }
+}
+
+
+// resize
+ground.addEventListener('mousemove',debounce((e)=>{
+  console.log(e.offsetX, e.offsetY);
+
+  let posX = e.offsetX;
+  let posY = e.offsetY;
+
+  const emotion = /* html */`<div class="emotion" style="left:${posX}px;top:${posY}px">😘</div>`  
+
+  insertLast(ground,emotion);
+  
+},1000))
+
+
 // second.addEventListener('click',function(){
 //   first.removeEventListener('click', handler);
 // })
-
-
 
 
 
--- client/chapter/dom/07.bubble.js
+++ client/chapter/dom/07.bubble.js
@@ -4,35 +4,47 @@
 
 
 /* 버블링 ----------------------------------------------------------------- */
+
+
 const visual = getNode('.visual');
 const news = getNode('.news');
 const desc = getNode('.desc');
 
-visual.addEventListener('click',(e)=>{
-  let elem = e.currentTarget;
-  // console.log(e.target);
-  // console.log(e.currentTarget);
-  console.log('%c visual','background:dodgerblue');
-  
 
-  css('.pop','display','block');
+visual.addEventListener('click',(e) => {
+  let elem = e.currentTarget;
+    // console.log('target : ',e.target);
+    // console.log('currentTarget :  ' ,e.currentTarget);
+    // console.log(this === e.currentTarget);
+    // console.log(this);
+    console.log('%c visual','background:dodgerblue');
+
+    css('.pop','display','block');
 })
+
 
 
 getNode('.pop').addEventListener('click',(e)=>{
-  e.stopPropagation();
-  css('.pop','display','none')
-})
-
-/* news.addEventListener('click',function(e){
   e.stopPropagation()
-  console.log('%c news','background:orange;color:white');
+  css('.pop','display','none');
 })
 
+/* 
+news.addEventListener('click',function(){
+    console.log('%c news','background:orange');
+})
 
 desc.addEventListener('click',function(e){
-  e.stopPropagation()
-  console.log('%c desc','background:hotpink;color:white');
-}) */
+    e.stopPropagation()
+    console.log('%c desc','background:hotpink');
+})
+
+ */
+
+
+
+
+
+
 
 /* 캡처링 ----------------------------------------------------------------- */
