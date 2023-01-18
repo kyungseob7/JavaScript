943f75d8de93cfd42f66d2e74666a6b7c73b6c5b
kyungseob7 <kseob7@naver.com>
2023-01-18 22:45:36 +0900

    20230118완료!

--- client/main.js
+++ client/main.js
@@ -1,23 +1,43 @@
 
-import { insertLast, xhrData } from "./lib/index.js"
+
+
+
+import { 
+  xhrData,
+  insertLast,
+  xhrPromise,
+  tiger
+ } from "./lib/index.js";
 
 
 
 
 
-/* 
-xhrData.get(
+
+// xhrPromise
+// .get('https://jsonplaceholder.typicode.com/users/1')
+// .then((res)=>{
+//   insertLast(document.body,JSON.stringify(res));
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+
+
+/* xhrData.get(
   'https://jsonplaceholder.typicode.com/users/1',
   (res)=>{
     insertLast('body',JSON.stringify(res))
   },
   (err)=>{
-    
     insertLast('body','데이터 로딩에 실패했습니다.')
   }
 )
+
  */
 
+/* 
 
 
-
+ */
