c4a6efbb457ce4910622c93234fe120df9e535f6
kyungseob7 <kseob7@naver.com>
2023-01-19 19:00:56 +0900

    20230119 완료!

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
-console.log('aa');
+import { getNode, saveStorage,loadStorage } from "./lib/index.js";
+
+
+const textField = getNode('#textField')
+const deleteButton = getNode('input[value="삭제"]')
+
+
+loadStorage('area').then((res)=>{
+  textField.value = res;
+})
+
+function inputHandler(){
+  saveStorage('area',textField.value)
+}
+
+
+textField.addEventListener('input',inputHandler)
